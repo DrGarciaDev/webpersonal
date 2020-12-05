efb90d8c20eb5bf4c2c03cc96aebdbca45dd1afa
Luis Alberto García Rodríguez <luis_grdz@hotmail.com>
2020-12-04 18:52:45 -0600

    Video 9: extendiendo la App Core

--- core/views.py
+++ core/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render, HttpResponse
 
+html_base = '''
+<h1>Mi web personal</h1>
+<ul>
+    <li><a href="/">Portada</a></li>
+    <li><a href="/about-me">Acerca de</a></li>
+</UL>
+'''
 # Create your views here.
 def home(request):
-    html_response = '<h1>Mi web personal</h1>'
+    return HttpResponse(html_base + '''
+                                        <h2>Portada</h2>
+                                        <p>Esto es la portada</p>
+                                    ''')
 
-    for i in range(10):
-        html_response += '<h2>Portada</h2>'
-    return HttpResponse(html_response)
+def about(request):
+    return HttpResponse(html_base + '''
+                                        <h2>Acerca de</h2>
+                                        <p>Me llamo Luis y estoy aprendiendo Django</p>
+                                    ''')
