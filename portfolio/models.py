485f0ef929204040057927fd4f55b5d2d13e2720
Luis Alberto García Rodríguez <luis_grdz@hotmail.com>
2020-12-10 14:21:13 -0600

    Limpieza de variable y ordenamiento en igualación de valores de campos del model Project

--- portfolio/models.py
+++ portfolio/models.py
@@ -2,8 +2,8 @@ from django.db import models
 
 # Create your models here.
 class Project(models.Model):
-    title = models.CharField(max_length=200)
+    title       = models.CharField(max_length=200)
     description = models.TextField()
-    image = models.ImageField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    image       = models.ImageField()
+    created     = models.DateTimeField(auto_now_add=True)
+    updated     = models.DateTimeField(auto_now=True)
